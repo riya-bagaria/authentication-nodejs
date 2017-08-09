7d51582517e9eb5b0d9f896c585f491630e0e288
riya-bagaria <riya.bagaria2015@gmail.com>
2017-08-09 18:06:58 +0530

    login logout funtionality

--- app.js
+++ app.js
@@ -16,23 +16,75 @@ app.use(require("express-session")({
 }));
 
 app.set('view engine','ejs');
+app.use(bodyParser.urlencoded({extended:true}));
 app.use(passport.initialize());
 app.use(passport.session());
 
+passport.use(new LocalStrategy(User.authenticate()));
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
-app.get("/",function(req,res){
-   
+//================================================
+//ROUTES
+//================================================
+app.get("/",function(req,res){   
         res.render("home");
-
 });
 
-app.get("/secret",function(req,res){
-   
+app.get("/secret",isLoggedIn,function(req,res){
         res.render("secret");
-
 });
+
+//AUTH ROUTES
+
+//SIGN up uth
+app.get("/register",function(req,res){
+    res.render("register");
+});
+
+//HANDLING USER SIGN UP
+app.post("/register",function(req,res){
+    User.register(new User({username:req.body.username}),req.body.password,function(err,user){
+        if(err){
+            console.log(err);
+            return res.render('register');
+        }
+        passport.authenticate("local")(req,res,function(){
+                res.redirect("/secret");
+        });
+    });
+});
+
+//LOGIN FORM
+app.get("/login",function(req,res){
+    res.render("login");
+});
+//login logic
+app.post("/login",passport.authenticate("local",{
+    successRedirect:"/secret",
+    failureRedirect:"/login"
+}),function(req,res){
+if(err){
+    console.log(err);
+}
+else{
+
+}
+});
+
+//logout
+app.get("/logout",function(req,res){
+      req.logout();
+      res.redirect("/");
+});
+
+function isLoggedIn(req,res,next){
+    if(req.isAuthenticated()){
+        return next();
+    }
+    res.redirect("/login");
+}
+
 app.listen(3000,function(){
 
         console.log("succesfully done!!");
